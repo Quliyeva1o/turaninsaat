0d5406249fa35cb4d91c5eb949650993e8ef2aa6
Quliyeva1o <renaquliyeva646@gmail.com>
2026-02-13 03:43:16 -0800

    aos

--- app/providers/AOSProvider.tsx
+++ app/providers/AOSProvider.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import AOS from "aos";
-import "aos/dist/aos.css";
+// import "aos/dist/aos.css";
 
 export default function AOSProvider() {
   useEffect(() => {
